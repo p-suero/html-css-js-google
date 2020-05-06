aa5822a929002ec089daad0fe09dbcf09f7d0d12
Paolo <paolo.suero@gmail.com>
2020-05-06 16:30:35 +0200

    Progetto terminato

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
+//setto il collegamento evitando che mi ricarichi la pagina al click
+$("a").click(function (event) {
+    event.preventDefault();
+})
+
 //OPZIONE A
 //intercetto il click sul collegamento
-$("#app-grid a").click(function(){
+$("#app-grid a").click(function () {
     //seleziono il dropdown
     var dropdown = $(".app-dropdown");
     //gli assegno il toogleClass (aggiunge e rimuove la clsse in automatico)
     dropdown.toggleClass("active");
 })
 
-// //OPZIONE NON TROPPO ELEGANTE(perchè sto aggiungendo css in linea)
+// //OPZIONE B (non troppo elegante perchè sto aggiungendo css in linea)
 // //intercetto il click sull'icona delle applicazioni
 // $("#app-grid a").click(function(){
 //     //seleziono il dropdown
@@ -16,7 +21,7 @@ $("#app-grid a").click(function(){
 //     dropdown.toggle();
 // })
 
-//chiudo il dropdown quando clicco in punto al di fuori di questo
+//chiudo il dropdown quando clicco in un punto al di fuori di questo
 //interceto il click sul document
 $(document).click(function(event) {
     //intercetto il click sull'elemento specifico cliccato nel document
